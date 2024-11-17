5863c8b1252160fd842ae2bad01b7541cce5ba7d
Maryam Bril <bmaryam@ept.sn>
2024-11-17 13:37:13 +0100

    push3

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+import { FooterComponent } from "../footer/footer.component";
+import { PreloaderComponent } from "../preloader/preloader.component";
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent, PreloaderComponent],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css'
 })
